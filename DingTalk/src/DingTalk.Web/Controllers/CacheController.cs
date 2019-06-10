0df2f88f66d6134aebc827e876966845755e111d
杨勇 <530521314@qq.com>
2019-06-10 21:50:05 +0800

    将钉钉测试项目移入到百宝箱中

---
+++ DingTalk/src/DingTalk.Web/Controllers/CacheController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using DingTalk.Web.Common;
+
+namespace DingTalk.Web.Controllers
+{
+    public class CacheController : Controller
+    {
+        private IMemoryCache _cache;
+        public CacheController(IMemoryCache cache)
+        {
+            _cache = cache;
+        }
+
+        [HttpGet]
+        [Route(nameof(TryGetValueAndSet))]
+        public ActionResult TryGetValueAndSet()
+        {
+            DateTime cacheEntry;
+
+            // Look for cache key.
+            if (!_cache.TryGetValue(CacheKeys.Entry, out cacheEntry))
+            {
+                // Key not in cache, so get data.
+                cacheEntry = DateTime.Now;
+                // Set cache options.
+                var cacheEntryOptions = new MemoryCacheEntryOptions()
+                // Keep in cache for this time, reset time if accessed.
+                .SetSlidingExpiration(TimeSpan.FromSeconds(3));
+                // Save data in cache.
+                _cache.Set(CacheKeys.Entry, cacheEntry, cacheEntryOptions);
+            }
+
+            string dd_accesstoken = string.Empty;
+            if (!_cache.TryGetValue(CacheKeys.DingTalkEntry, out dd_accesstoken))
+            {
+                // Key not in cache, so get data.
+                dd_accesstoken = "test" + DateTime.Now.ToShortTimeString();
+                // Set cache options.
+                var cacheEntryOptions = new MemoryCacheEntryOptions()
+                // Keep in cache for this time, reset time if accessed.
+                .SetSlidingExpiration(TimeSpan.FromSeconds(3));
+                // Save data in cache.
+                _cache.Set(CacheKeys.DingTalkEntry, dd_accesstoken, cacheEntryOptions);
+            }
+
+            return Ok(new { cacheEntry, dd_accesstoken });
+        }
+
+        [HttpGet]
+        [Route(nameof(CacheGetOrCreate))]
+        public IActionResult CacheGetOrCreate()
+        {
+            var cacheEntry = _cache.GetOrCreate(CacheKeys.Entry, entry =>
+            {
+                entry.SlidingExpiration = TimeSpan.FromSeconds(3);
+                return DateTime.Now;
+            });
+
+            return Ok(cacheEntry);
+        }
+
+        [HttpGet]
+        [Route(nameof(CacheGetOrCreateAsync))]
+        public async Task<IActionResult> CacheGetOrCreateAsync()
+        {
+            var cacheEntry = await _cache.GetOrCreateAsync(CacheKeys.Entry, entry =>
+            {
+                entry.SlidingExpiration = TimeSpan.FromSeconds(3);
+                return Task.FromResult(DateTime.Now);
+            });
+
+            return Ok(cacheEntry);
+        }
+
+        [HttpGet]
+        [Route(nameof(CacheGet))]
+        public IActionResult CacheGet()
+        {
+            var cacheEntry = _cache.Get<DateTime?>(CacheKeys.Entry);
+            return Ok(cacheEntry);
+        }
+    }
+}
---
+++ DingTalk/src/DingTalk.Web/Controllers/HttpClientController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DingTalk.Web.Models;
+
+namespace DingTalk.Web.Controllers
+{
+    public class HttpClientController : Controller
+    {
+        IHttpClientFactory _httpClientFactory;
+
+        public HttpClientController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet]
+        [Route(nameof(Index))]
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var result = await client.GetAsync("http://aspnetcore.online/api/resource/getresource");
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Route(nameof(GetWebResource))]
+        public async Task<IActionResult> GetWebResource()
+        {
+            //do something...
+            using (var httpClient = new HttpClient())
+            {
+                var requestUri = "http://aspnetcore.online/api/resource/getresource";
+                var httpResponseMessage = await httpClient.GetAsync(requestUri);
+                //do something...
+
+                return Ok(httpResponseMessage);
+            }
+        }
+
+        [HttpGet]
+        [Route(nameof(PostWebResource))]
+        public async Task<IActionResult> PostWebResource()
+        {
+            //do something...
+            using (var httpClient = new HttpClient())
+            {
+                var requestUri = "http://aspnetcore.online/api/resource/postresource";
+                var httpResponseMessage = await httpClient.PostAsJsonAsync(requestUri, "星城软件");
+                //do something...
+
+                return Ok(httpResponseMessage);
+            }
+        }
+    }
+}
