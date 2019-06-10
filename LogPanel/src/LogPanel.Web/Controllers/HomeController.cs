eb10d1ed57c0030f739e68a98292a9d28f56852d
杨勇 <530521314@qq.com>
2019-06-10 21:20:46 +0800

    从日志可视化面板单独项目迁移到百宝箱项目中

--- LogPanel/src/LogPanel.Web/Controllers/HomeController.cs
+++ LogPanel/src/LogPanel.Web/Controllers/HomeController.cs
@@ -5,22 +5,63 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using LogPanel.Web.Models;
+using Microsoft.Extensions.Logging;
+using System.Text;
+using Serilog;
 
 namespace LogPanel.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
         public IActionResult Index()
         {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            _logger.LogDebug($"开始监控整个网站的日志信息_Debug{DateTime.Now}");
+            _logger.LogInformation($"开始监控整个网站的日志信息Information_{DateTime.Now}");
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult About()
         {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+            ViewData["Message"] = "Your application description page.";
+
+            try
+            {
+                _logger.LogInformation($"业务逻辑执行前，运行正常");
+                //执行其它业务逻辑
+                int textInt = 5;
+                int result = textInt / 0;
+                //执行其它业务逻辑
+                _logger.LogInformation($"业务逻辑执行后，运行正常");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"出现异常,异常信息{ex.Message}");
+                throw;
+            }
+
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Contact()
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            Log.Debug("Log.Debug:Serilog Start Send Message");
+            Log.Information("Log.Information:Serilog Start send Message");
+            Log.Logger.Debug("Log.Logger:Serilog Start Send Message");
+            Log.Logger.Information("Log.Logger.Information:Serilog Start Send Message");
+            return View();
+        }
+
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
--- LogPanel/src/LogPanel.Web/Startup.cs
+++ LogPanel/src/LogPanel.Web/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace LogPanel.Web
 {
@@ -17,6 +19,14 @@ namespace LogPanel.Web
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+            //方式一：配置Seq服务器的地址(5341端口为默认地址)
+            //Log.Logger = new LoggerConfiguration()
+            //    .MinimumLevel.Debug()
+            //    .MinimumLevel.Override("LoggingService", Serilog.Events.LogEventLevel.Debug)
+            //    .Enrich.FromLogContext()
+            //    .WriteTo.Seq("http://localhost:5341")
+            //    .WriteTo.LiterateConsole()
+            //    .CreateLogger();
         }
 
         public IConfiguration Configuration { get; }
@@ -31,13 +41,46 @@ namespace LogPanel.Web
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
+            //方式二
+            //var serilog = new LoggerConfiguration()
+            //        .MinimumLevel.Information()
+            //        .MinimumLevel.Override("LoggingService", Serilog.Events.LogEventLevel.Debug)
+            //        .Enrich.FromLogContext()
+            //        .WriteTo.Seq("http://localhost:5341")
+            //        .WriteTo.LiterateConsole();
+
+            //services.AddLogging(loggerBuilder =>
+            //{
+            //    loggerBuilder.AddSerilog(serilog.CreateLogger());
+            //});
+
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            //默认日志功能
+            //loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            //loggerFactory.AddDebug();
+
+            #region Serilog
+            //方式三
+            //默认配置
+            //loggerFactory.AddSerilog();
+
+            //配置Seq服务器的地址(5341端口为默认地址)
+            //注意，实际发布后的地址为"域名或IP:端口"
+            var serilog = new LoggerConfiguration()
+                .MinimumLevel.Information()
+                .MinimumLevel.Override("LoggingService", Serilog.Events.LogEventLevel.Debug)
+                .Enrich.FromLogContext()
+                .WriteTo.Seq("http://localhost:5341")
+                .WriteTo.LiterateConsole();
+            loggerFactory.AddSerilog(serilog.CreateLogger());
+            #endregion
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
