288ea7edaeef11ce596b554af7fab119fe6aba80
杨勇 <530521314@qq.com>
2019-06-10 21:55:12 +0800

    将多地图呈现项目移入到百宝箱中

---
+++ HawkEye/src/HawkEye.Web/Controllers/MapsController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HawkEye.Web.Controllers
+{
+    public class MapsController : Controller
+    {
+        /// <summary>
+        /// 高德地图
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult AliMap()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 百度地图
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult BaiduMap()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 腾讯地图
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult TencentMap()
+        {
+            return View();
+        }
+    }
+}
---
+++ HawkEye/src/HawkEye.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HawkEye.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
