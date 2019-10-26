09d7557ec654cb63933a23b532f16dfd1c4997f5
杨勇 <530521314@qq.com>
2019-10-26 12:17:08 +0800

    增加状态机案例

---
+++ StateMachine/src/StateMachine.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace StateMachine.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
