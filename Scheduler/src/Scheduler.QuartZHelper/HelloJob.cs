8790d306d900e12fb9f6b206e6fb744d88b7e547
杨勇 <530521314@qq.com>
2020-03-22 14:31:31 +0800

    增加WPS对接Demo

---
+++ Scheduler/src/Scheduler.QuartZHelper/HelloJob.cs
@@ -0,0 +1,16 @@
+﻿using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scheduler.QuartZHelper
+{
+    public class HelloJob : IJob
+    {
+        public async Task Execute(IJobExecutionContext context)
+        {
+            await Console.Out.WriteLineAsync("Greetings from HelloJob!" + DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss"));
+        }
+    }
+}
---
+++ Scheduler/src/Scheduler.QuartZHelper/Program.cs
@@ -0,0 +1,65 @@
+﻿using Quartz;
+using Quartz.Impl;
+using Quartz.Logging;
+using System;
+using System.Collections.Specialized;
+using System.Threading.Tasks;
+
+namespace Scheduler.QuartZHelper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LogProvider.SetCurrentLogProvider(new ConsoleLogProvider());
+
+            RunProgram().GetAwaiter().GetResult();
+
+            Console.WriteLine("Press any key to close the application");
+            Console.ReadKey();
+        }
+
+        private static async Task RunProgram()
+        {
+            try
+            {
+                // Grab the Scheduler instance from the Factory
+                NameValueCollection props = new NameValueCollection
+                {
+                    { "quartz.serializer.type", "binary" }
+                };
+                StdSchedulerFactory factory = new StdSchedulerFactory(props);
+                IScheduler scheduler = await factory.GetScheduler();
+
+                // define the job and tie it to our HelloJob class
+                IJobDetail job = JobBuilder.Create<HelloJob>()
+                    .WithIdentity("myJob", "group1")
+                    .Build();
+
+                // Trigger the job to run now, and then every 40 seconds
+                ITrigger trigger = TriggerBuilder.Create()
+                    .WithIdentity("myTrigger", "group1")
+                    .StartNow()
+                    .WithSimpleSchedule(x => x
+                        .WithIntervalInSeconds(2)
+                        .RepeatForever())
+                    .Build();
+
+                await scheduler.ScheduleJob(job, trigger);
+
+                // and start it off
+                await scheduler.Start();
+
+                // some sleep to show what's happening
+                await Task.Delay(TimeSpan.FromSeconds(20));
+
+                // and last shut down the scheduler when you are ready to close your program
+                await scheduler.Shutdown();
+            }
+            catch (SchedulerException se)
+            {
+                await Console.Error.WriteLineAsync(se.ToString());
+            }
+        }
+    }
+}
