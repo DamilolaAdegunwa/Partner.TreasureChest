254576c41313d6838432e3399e1f4121c3d313f8
杨勇 <530521314@qq.com>
2020-04-16 22:06:03 +0800

    增加两种Redis的操作方式，使用同一种Nuget包，封装方式不同，简单与复杂

---
+++ RedisOperate/src/RedisOperate.RedisBase/AppConfigurationServices.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+
+namespace RedisOperate.RedisTool
+{
+    /// <summary>
+    /// 读取appsetting.json
+    /// </summary>
+    public class AppConfigurataionServices
+    {
+        public static IConfiguration Configuration { get; set; }
+        static AppConfigurataionServices()
+        {
+            //ReloadOnChange = true 当appsettings.json被修改时重新加载
+            Configuration = new ConfigurationBuilder()
+                .Add(new JsonConfigurationSource { Path = "appsettings.json", ReloadOnChange = true })
+                .Build();
+        }
+    }
+}
---
+++ RedisOperate/src/RedisOperate.Tool/RedisService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Logging;
+using StackExchange.Redis;
+using Newtonsoft.Json;
+
+namespace RedisOperate.Tool
+{
+    public class RedisService
+    {
+        public static RedisManager Redis
+        {
+            get
+            {
+                return new RedisManager();
+            }
+        }
+    }
+}
